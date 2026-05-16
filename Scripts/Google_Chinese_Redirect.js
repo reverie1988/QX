b05628d250e7cd80f19812426e483a0a77bb7e73
明亮的夜空 <102882768+reverie1988@users.noreply.github.com>
2026-05-16 13:56:20 +0800

    Update Google_Chinese_Redirect.js

--- Scripts/Google_Chinese_Redirect.js
+++ Scripts/Google_Chinese_Redirect.js
@@ -15,7 +15,7 @@ const LANGUAGE = "zh-CN";
 // "HK" = 偏香港地区
 // "TW" = 偏台湾地区
 // "CN" = 偏中国大陆地区
-const REGION = "TW";
+const REGION = "CN";
 
 const rawUrl = $request.url;
 const url = new URL(rawUrl);
